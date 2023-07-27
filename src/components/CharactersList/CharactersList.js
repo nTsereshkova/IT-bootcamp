ae4f02ef26574e6aa73f0a216917629069e10fc0
Терешкова Анастасия Игоревна <@emz.local>
2023-07-27 10:55:50 +0300

    use effects to scroll

--- src/components/CharactersList/CharactersList.js
+++ src/components/CharactersList/CharactersList.js
@@ -7,6 +7,7 @@ import {
   changeCurrentPage,
 } from '../../store/actions/actions';
 import { Character } from './Character';
+import { AiOutlineArrowUp } from 'react-icons/ai';
 import './CharactersList.css';
 
 const CharacterList = () => {
@@ -17,11 +18,12 @@ const CharacterList = () => {
 
   const [loading, setLoading] = useState(false);
   const [firstLoad, setFirstLoad] = useState(true);
+  const [showBackToTop, setShowbackToTop] = useState(false);
 
   console.log(currentPage);
 
   useEffect(() => {
-    console.log('юз эффект который слежит долистал ли ты до конца ');
+    console.log('юз эффект который првоеряет проктрукту вниз');
     console.log(infiniteFetchMode);
     if (infiniteFetchMode === true) {
       console.log('ты в режиме infiniteFetchMode');
@@ -30,9 +32,19 @@ const CharacterList = () => {
           window.innerHeight + document.documentElement.scrollTop !==
           document.documentElement.offsetHeight
         ) {
+          //прячем кнопку
+          if (document.documentElement.scrollTop < 400) {
+            setShowbackToTop(false);
+          }
+          // показываем кнопку (если был скролл вверх, потом снова назад)
+          if (document.documentElement.scrollTop > 400) {
+            setShowbackToTop(true);
+          }
           return;
         }
         console.log('ты долистал до конца');
+
+        setShowbackToTop(true);
         dispatch(changeCurrentPage(currentPage));
         setLoading(true);
       };
@@ -41,6 +53,38 @@ const CharacterList = () => {
     }
   });
   // [infiniteFetchMode, dispatch]
+  // useEffect(() => {
+  //   console.log('эффект пряет кнопку');
+  //   const scrollHandler =() => {
+
+  //   }
+  //   if (infiniteFetchMode) {
+  //     if (document.documentElement.scrollTop > 400) {
+  //       setShowbackToTop(true);
+  //     }
+  //     if (pageYOffset < document.documentElement.clientHeight) {
+  //       console.log('спрятали кнопку ');
+  //       setShowbackToTop(false);
+  //     }
+  //   }
+  // });
+
+  // useEffect(() => {
+  //   console.log('use effect который проверяет прокруту вверх');
+  //   if (infiniteFetchMode) {
+  //     console.log('ты в режиме infiniteFetchMode  в прокрутке вверх');
+  //     const scrollHandler = () => {
+  //       console.log(currentPage, 'current page scrollhadler');
+  //       if (document.documentElement.scrollTop === 0 && currentPage > 1) {
+  //         console.log('ты долистал до самого верха ');
+  //         dispatch(changeCurrentPage(currentPage - 1));
+  //         setLoading(true);
+  //       }
+  //     };
+  //     window.addEventListener('scroll', scrollHandler);
+  //     return () => window.removeEventListener('scroll', scrollHandler);
+  //   }
+  // });
 
   useEffect(() => {
     console.log('юз эффект ответсвенный за запрос на сервер');
@@ -48,12 +92,17 @@ const CharacterList = () => {
     if (loading && infiniteFetchMode) {
       console.log('попали в  loading && infiniteFetchMode');
       dispatch(infiniteFetchCharacters(currentPage));
-      return setLoading(false);
-      // if (currentPage * 20 >= characters.length) {
-      //   console.log('currentPage * 20 >= characters.length');
-      //   // dispatch(infiniteFetchCharacters(currentPage));
+      if (currentPage * 20 >= characters.length) {
+        console.log('currentPage * 20 >= characters.length');
+        dispatch(changeCurrentPage(currentPage + 1));
+        // return setLoading(false);
+      }
+      // if (currentPage * 20 <= characters.length) {
+      //   console.log('currentPage * 20 <= characters.length');
+      //   dispatch(changeCurrentPage(currentPage - 1));
       //   // return setLoading(false);
       // }
+      return setLoading(false);
     }
     if (infiniteFetchMode == false) {
       console.log('!loading && !infiniteFetchMode');
@@ -81,11 +130,26 @@ const CharacterList = () => {
   //     setFirstLoad(true);
   //   }
   // }, [loading, infiniteFetchMode, currentPage]);
+  console.log(showBackToTop, 'show back to top ');
+
+  const goBackToTopHandler = () => {
+    window.scrollTo(pageXOffset, 0);
+  };
+
   return (
     <div className="charactersList">
       {characters.map(item => (
         <Character key={uuidv4()} item={item} />
       ))}
+      {infiniteFetchMode && showBackToTop && (
+        <button className="charactersList-btn">
+          {/* Back to top  */}
+          <AiOutlineArrowUp
+            className="charactersList-btn-icon"
+            onClick={goBackToTopHandler}
+          />
+        </button>
+      )}
     </div>
   );
 };
